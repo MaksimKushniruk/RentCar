b3a368567a1085f2fd75a24799a1b2496ffde866
Reiysher <reiysher25@gmail.com>
2020-11-15 13:19:47 +0300

    Added requets model

--- Rent/Enums.cs
+++ Rent/Enums.cs
@@ -7,4 +7,20 @@ namespace Rent
         Свободен = 0,
         Арендован = 1
     }
+    // Дописать полный пул цветов
+    public enum CarColor
+    {
+        Серый = 0,
+        Белый = 1
+    }
+
+    public enum CarBrand
+    {
+
+    }
+
+    public enum MercedesModels
+    {
+
+    }
 }

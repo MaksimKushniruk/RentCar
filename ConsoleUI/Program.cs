200475b36cca7cfaaa6ca8119b78cdf69dfdd9f1
Reiysher <reiysher25@gmail.com>
2020-11-21 19:33:00 +0300

    Dynamic menu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -13,18 +13,41 @@ namespace ConsoleUI
             while (true)
             {
                 Console.Clear();
-                ConsoleLogo("Вас приветствует сервис аренды машин.");
+                ConsoleMenu.Logo("Вас приветствует сервис аренды машин.");
                 Console.WriteLine("\nДля продолжения нажмите любую клавишу...");
                 Console.ReadKey();
                 Console.Clear();
-                ConsoleLogo("Выберите действие.");
-                ConsoleMainMenu("1. Аренда", "2. Администрирование");
+                ConsoleMenu.Logo("Выберите действие.");
+                ConsoleMenu.MainMenu(new List<string> { "Аренда", "Администрирование" });
                 switch (Console.ReadKey().KeyChar)
                 {
                     case '1':
                         Console.Clear();
-                        ConsoleLogo("Аренда");
-                        RentMenu();
+                        ConsoleMenu.Logo("Аренда");
+                        ConsoleMenu.Menu(new List<string> { "Новый заказ", "Редактировать заказ", "Отменить заказ ", "Найти заказ" });
+                        switch (Console.ReadKey().KeyChar)
+                        {
+                            case '1':
+                                Console.Clear();
+                                ConsoleMenu.Logo("Новый заказ");
+                                ConsoleMenu.Menu(new List<string> { "Новый клиент ", "Выбрать клиента", "Меню 3 ", "меню 4 " });
+                                break;
+                            case '2':
+                                Console.Clear();
+                                ConsoleMenu.Logo("Редактировать заказ");
+                                ConsoleMenu.Menu(new List<string> { "Меню 1 ", "Меню 2 ", "Меню 3 ", "меню 4 " });
+                                break;
+                            case '3':
+                                Console.Clear();
+                                ConsoleMenu.Logo("Отменить заказ ");
+                                ConsoleMenu.Menu(new List<string> { "Меню 1 ", "Меню 2 ", "Меню 3 ", "меню 4 " });
+                                break;
+                            case '4':
+                                Console.Clear();
+                                ConsoleMenu.Logo("Найти заказ");
+                                ConsoleMenu.Menu(new List<string> { "Меню 1 ", "Меню 2 ", "Меню 3 ", "меню 4 " });
+                                break;
+                        }
                         break;
                     case '2':
                         break;
@@ -34,8 +57,24 @@ namespace ConsoleUI
                 Console.ReadKey();
             }
         }
+    }
+    class ConsoleMenu
+    {
+        // Сделать динамично
+        public static void MainMenu(List<string> menu)
+        {
+            for (int i = 0; i < menu.Count; i++)
+            {
+                string textMenu = $"{i + 1}. {menu[i]}";
+                Console.Write(new string(' ', ((Console.WindowWidth / 2) - textMenu.Length) / 2));
+                Console.Write(textMenu);
+                Console.Write(new string(' ', ((Console.WindowWidth / 2) - textMenu.Length) / 2));
+                Console.Write("|");
+            }
+            Console.WriteLine(new string('-', Console.WindowWidth));
+        }
 
-        static void ConsoleLogo(string message)
+        public static void Logo(string message)
         {
             Console.WriteLine(new string('-', Console.WindowWidth));
             Console.Write(new string(' ', (Console.WindowWidth - message.Length) / 2));
@@ -43,48 +82,18 @@ namespace ConsoleUI
             Console.WriteLine(new string('-', Console.WindowWidth));
         }
 
-        static void ConsoleMainMenu(string rentActions, string adminActions)
+        public static void Menu(List<string> menu)
         {
-            Console.Write(new string(' ', ((Console.WindowWidth / 2) - rentActions.Length) / 2));
-            Console.Write(rentActions);
-            Console.Write(new string(' ', ((Console.WindowWidth / 2) - rentActions.Length) / 2));
-            Console.Write("|");
-            Console.Write(new string(' ', ((Console.WindowWidth / 2) - adminActions.Length) / 2));
-            Console.Write(adminActions);
-            Console.Write(new string(' ', ((Console.WindowWidth / 2) - adminActions.Length) / 2));
-            Console.WriteLine(new string('-', Console.WindowWidth));
-        }
+            for (int i = 0; i < menu.Count; i++)
+            {
+                Console.Write($"{i + 1}.");
+                Console.Write(new string(' ', (((Console.WindowWidth / 2) - menu[i].Length) / 2) - 2));
+                Console.Write(menu[i]);
+                Console.Write(new string(' ', ((Console.WindowWidth / 2) - menu[i].Length) / 2));
+                Console.WriteLine("|");
+                Console.WriteLine(new string('-', Console.WindowWidth / 2));
 
-        static void RentMenu()
-        {
-            string newRent = "Новый заказ";
-            Console.Write("1.");
-            Console.Write(new string(' ', (((Console.WindowWidth / 2) - newRent.Length) / 2) - 2));
-            Console.Write(newRent);
-            Console.Write(new string(' ', ((Console.WindowWidth / 2) - newRent.Length) / 2));
-            Console.WriteLine("|");
-            Console.WriteLine(new string('-', Console.WindowWidth / 2));
-            string editRent = "Редактировать заказ";
-            Console.Write("2.");
-            Console.Write(new string(' ', (((Console.WindowWidth / 2) - editRent.Length) / 2) - 2));
-            Console.Write(editRent);
-            Console.Write(new string(' ', ((Console.WindowWidth / 2) - editRent.Length) / 2));
-            Console.WriteLine("|");
-            Console.WriteLine(new string('-', Console.WindowWidth / 2));
-            string deleteRent = "Отменить заказ";
-            Console.Write("3.");
-            Console.Write(new string(' ', (((Console.WindowWidth / 2) - deleteRent.Length) / 2) - 3));
-            Console.Write(deleteRent);
-            Console.Write(new string(' ', ((Console.WindowWidth / 2) - deleteRent.Length) / 2));
-            Console.WriteLine("|");
-            Console.WriteLine(new string('-', Console.WindowWidth / 2));
-            string getRent = "Найти заказ";
-            Console.Write("4.");
-            Console.Write(new string(' ', (((Console.WindowWidth / 2) - getRent.Length) / 2) - 2));
-            Console.Write(getRent);
-            Console.Write(new string(' ', ((Console.WindowWidth / 2) - getRent.Length) / 2));
-            Console.WriteLine("|");
-            Console.WriteLine(new string('-', Console.WindowWidth / 2));
+            }
             Console.Write("\nДля продолжения сделайте свой выбор...");
         }
     }

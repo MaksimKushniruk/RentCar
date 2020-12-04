3cc523271d707b1c740ed4dfc5f0650ca152ad51
Reiysher <reiysher25@gmail.com>
2020-12-04 07:35:53 +0300

    reservation service documentation

--- Rent/Services/ReservationService.cs
+++ Rent/Services/ReservationService.cs
@@ -13,14 +13,29 @@ namespace Rent.Services
         {
             reservationRepository = new ReservationRepository();
         }
+        /// <summary>
+        /// Reservation service for creating object. Returns Id of created Reservation in database.
+        /// </summary>
+        /// <param name="reservation"></param>
+        /// <returns></returns>
         public int CreateReservation(Reservation reservation)
         {
             return reservationRepository.AddReservation(reservation);
         }
+        /// <summary>
+        /// Reservation service for deleting object. Returns bool result of operation.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public bool DeleteReservation(int id)
         {
             return reservationRepository.DeleteReservation(id);
         }
+        /// <summary>
+        /// Reservation search in database service. Returns List of Reservations.
+        /// </summary>
+        /// <param name="fields"></param>
+        /// <returns></returns>
         public List<Reservation> GetReservation(Dictionary<string, string> fields)
         {
             return reservationRepository.GetReservation(new ReservationRequest(
@@ -31,6 +46,12 @@ namespace Rent.Services
                     string.IsNullOrEmpty(fields["MinDate"]) ? (DateTime?)null : DateTime.Parse(fields["MinDate"]),
                     string.IsNullOrEmpty(fields["MaxDate"]) ? (DateTime?)null : DateTime.Parse(fields["MaxDate"])));
         }
+        /// <summary>
+        /// Reservation update service. Returns updated Reservation.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="fieldsForUpdate"></param>
+        /// <returns></returns>
         public Reservation UpdateReservation(int id, Dictionary<string, string> fieldsForUpdate)
         {
             List <Reservation> reservations = reservationRepository.GetReservation(new ReservationRequest { Id = id });

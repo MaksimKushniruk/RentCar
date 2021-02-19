2f8e5f6193820f062fa96c83c134ed8760965dab
Reiysher <reiysher25@gmail.com>
2021-02-19 16:14:06 +0300

    changed data  transfer objects for one to many relationship

--- Core/DTO/CustomerDto.cs
+++ Core/DTO/CustomerDto.cs
@@ -10,5 +10,10 @@ namespace Core.DTO
         public string LastName { get; set; }
         public string City { get; set; }
         public string PhoneNumber { get; set; }
+        public List<ReservationDto> Reservations { get; set; }
+        public CustomerDto()
+        {
+            Reservations = new List<ReservationDto>();
+        }
     }
 }
